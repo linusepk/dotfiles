3820d7282229a2e343700f1a85e45d05516f1f4d
Linus Kåreblom <linus.kareblom@gmail.com>
2024-08-18 18:16:53 +0200

    Fixed <leader>Y yanking the whole line and not from the cursor to the end.

--- general/.config/nvim/lua/linusepk/rebind.lua
+++ general/.config/nvim/lua/linusepk/rebind.lua
@@ -11,8 +11,7 @@ vim.keymap.set("n", "<C-f>", "<cmd>silent !tmux neww fzftmux<CR>");
 
 vim.keymap.set("n", "<leader>y", "\"+y");
 vim.keymap.set("v", "<leader>y", "\"+y");
-vim.keymap.set("n", "<leader>Y", "\"+Y");
-vim.keymap.set("v", "<leader>Y", "\"+Y");
+vim.keymap.set("n", "<leader>Y", "\"+y$");
 
 vim.keymap.set("n", "<leader>p", "\"+p");
 vim.keymap.set("v", "<leader>p", "\"+p");
