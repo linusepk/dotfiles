d10212587a6a21cc5b36b0b9e05e476e52c10862
Linus Kåreblom <linus.kareblom@gmail.com>
2024-10-02 18:41:25 +0200

    Removed format motion.

--- general/.config/nvim/lua/linusepk/plugins/lspconfig.lua
+++ general/.config/nvim/lua/linusepk/plugins/lspconfig.lua
@@ -44,9 +44,6 @@ return {
                 vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename, opts)
                 vim.keymap.set({ 'n', 'v' }, '<space>ca', vim.lsp.buf.code_action, opts)
                 vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
-                vim.keymap.set('n', '<space>f', function()
-                    vim.lsp.buf.format { async = true }
-                end, opts)
                 vim.keymap.set('n', '<F2>', vim.lsp.buf.rename, opts)
             end,
         })
